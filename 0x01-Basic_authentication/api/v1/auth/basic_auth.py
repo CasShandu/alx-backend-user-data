eb05a965d4d8e4a2bed4a7b9af6092f354c04c30
“CasShandu” <cas.shandu@gmail.com>
2024-11-12 17:02:46 +0200

    basic authentication to api

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,23 @@
 """
 Define basic_auth for the API
 """
+import re
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
-    """ BasicAuth class inheriting from Auth """
-    pass
-
+    """Basic authentication class.
+    """
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
