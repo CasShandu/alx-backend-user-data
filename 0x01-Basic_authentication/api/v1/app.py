3b808bfeda0cceb97b97c1e9487f181cc1c5dd71
“CasShandu” <cas.shandu@gmail.com>
2024-11-13 05:35:36 +0200

    pycodestyle errors

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,39 +8,41 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 
 from api.v1.auth.auth import Auth
-from api.v1.views import app_views
 from api.v1.auth.basic_auth import BasicAuth
-import os
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+auth_type = getenv("AUTH_TYPE", "basic")
+if auth_type == "basic":
+    auth = BasicAuth()
+else:
+    auth = Auth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """
-    Unauthorized handler.
-    """
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler.
-    """
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
+
 
 @app.before_request
 def authenticate_user():
-    """Authenticates a user before processing a request.
-    """
+    """ Authenticates a user before processing a request. """
     if auth:
         excluded_paths = [
             '/api/v1/status/',
@@ -49,13 +51,14 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
             if auth_header is None:
                 abort(401)
+            user = auth.current_user(request)
             if user is None:
                 abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
